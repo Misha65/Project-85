f9148ef51bf4330e540546ff7855af14875d0d35
Misha65 <73336746+Misha65@users.noreply.github.com>
2023-10-12 19:37:39 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRZFyHdgtXajW1bgxojpXKAlgGeb5GQ3s",
+  authDomain: "spectagram-d2da6.firebaseapp.com",
+  databaseURL: "https://spectagram-d2da6-default-rtdb.firebaseio.com",
+  projectId: "spectagram-d2da6",
+  storageBucket: "spectagram-d2da6.appspot.com",
+  messagingSenderId: "623076172393",
+  appId: "1:623076172393:web:876c734ae388531462b8f8"
+};
